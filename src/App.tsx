3642efdd18ecdd8a6cd0db735016017167676433
Roksolana Dutchak <dytchakro@gmail.com>
2023-04-28 16:19:31 +0300

    add config for prettier and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
 function App() {
-  const [greeting, setGreeting] = useState("");
-  type Greeting = string;
-  useEffect(() => {
-    axios.get<Greeting>('http://localhost:8000/ping',
-        {
-            headers: {
-                Accept: 'application/json',
-            },
-        }
-            )
-        .then(res => {
-            console.log(res.data);
-          setGreeting(res.data)
-
-        });
-  },[])
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-            {greeting}
-        </p>
-      </header>
-    </div>
-  );
+	const [greeting, setGreeting] = useState('');
+	type Greeting = string;
+	useEffect(() => {
+		axios
+			.get<Greeting>('http://localhost:8000/ping', {
+				headers: {
+					Accept: 'application/json',
+				},
+			})
+			.then((res) => {
+				console.log(res.data);
+				setGreeting(res.data);
+			});
+	}, []);
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>{greeting}</p>
+			</header>
+		</div>
+	);
 }
 
 export default App;
